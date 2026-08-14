9872cdce974de4de4f411ba51942b6de4058e3bc
“Dipesh <deeppatelpvt@gmail.com>
2026-08-13 21:02:13 -0600

    Update work experience company name
    Rename Frendeal Inc to Whistling Solutions on the experience timeline.

--- assets/swimlanesDiagram-G3AALYLV-BpUshJT1.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U-CaHaoVq-.js";import{_ as a}from"./mermaid.core-DyBchxch.js";import"./chunk-5VM5RSS4-CoJnCPt4.js";import"./chunk-XXDRQBXY-BhRZsYp7.js";import"./chunk-VR4S4FIN-DZHWVl-A.js";import"./chunk-32BRIVSS-_fWjevT6.js";import"./channel-BLgTmhmT.js";import"./index-att6otb_.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
---
+++ assets/swimlanesDiagram-G3AALYLV-CXjClczA.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U-BT3bMEfa.js";import{_ as a}from"./mermaid.core-BzL2kZXM.js";import"./chunk-5VM5RSS4-Dvk2U9_L.js";import"./chunk-XXDRQBXY-CUQhCGZr.js";import"./chunk-VR4S4FIN-Bi6Vnh9a.js";import"./chunk-32BRIVSS-Bis-iViv.js";import"./channel-Bi4rM6AS.js";import"./index-BzLLvmJQ.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
